97ad2c37207f8432bf9290eb83d21d5013765d7a
BackRunner <12205593+backrunner@users.noreply.github.com>
2020-05-17 18:43:55 +0800

    add: routes

--- app/router.js
+++ app/router.js
@@ -1,8 +1,12 @@
 'use strict';
 
+const api_version = 'v1';
+
 /**
  * @param {Egg.Application} app - egg application
  */
-module.exports = (app) => {
+module.exports = app => {
     const { router, controller } = app;
+    router.post(`/${api_version}/slice/submit`, controller[api_version].slice.submit);
+    router.get(`/${api_version}/slice/get`, controller[api_version].slice.get);
 };
